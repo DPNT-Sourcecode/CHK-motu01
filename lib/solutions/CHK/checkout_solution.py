bcfc867562ac94905319d6d868c80965976dda56
sbx_user1051 <sbx_user1051@169.254.16.175>
2025-04-14 14:15:39 +0000

    Mon Apr 14 14:15:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus: str) -> int:
         # Return -1 for error if no string or if it contains anything that isn't ABCD
-        ALLOWED_SKUS: str = "ABCD"
+        ALLOWED_SKUS: str = "ABCDE"
         if not set(skus) <= set(ALLOWED_SKUS):
             return -1
 
@@ -12,6 +12,10 @@ class CheckoutSolution:
         num_b = skus.count("B")
         num_c = skus.count("C")
         num_d = skus.count("D")
+        num_e = skus.count("E")
+
+        # Apply the E discount on number of B
+        num_b -= num_e // 2
 
         # Check for discounts
         num_a_discount_5, num_a = divmod(num_a, 5)
@@ -23,11 +27,13 @@ class CheckoutSolution:
         charge_b = num_b * 30
         charge_c = num_c * 20
         charge_d = num_d * 15
-        charge_a_discount = num_a_discount * 130
+        charge_e = num_e * 40
+        charge_a_discount_5 = num_a_discount_5 * 200
+        charge_a_discount_3 = num_a_discount_3 * 130
         charge_b_discount = num_b_discount * 45
 
         # Return charge
-        return charge_a + charge_b + charge_c + charge_d + charge_a_discount + charge_b_discount
+        return charge_a + charge_b + charge_c + charge_d + charge_e + charge_a_discount_3 + charge_a_discount_5 + charge_b_discount
 
 # +------+-------+------------------------+
 # | Item | Price | Special offers         |
@@ -38,3 +44,4 @@ class CheckoutSolution:
 # | D    | 15    |                        |
 # | E    | 40    | 2E get one B free      |
 # +------+-------+------------------------+
+
