53e52146dab1d59f809d303162bb6f4fbcc21b0a
sbx_user1051 <sbx_user1051@169.254.16.175>
2025-04-14 14:18:39 +0000

    Mon Apr 14 14:18:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,8 +14,8 @@ class CheckoutSolution:
         num_d = skus.count("D")
         num_e = skus.count("E")
 
-        # Apply the E discount on number of B
-        num_b -= num_e // 2
+        # Apply the E discount on number of B to a minimum B of 0
+        num_b -= max(num_e // 2, 0)
 
         # Check for discounts
         num_a_discount_5, num_a = divmod(num_a, 5)
@@ -44,4 +44,5 @@ class CheckoutSolution:
 # | D    | 15    |                        |
 # | E    | 40    | 2E get one B free      |
 # +------+-------+------------------------+
+
 
