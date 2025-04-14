f0f25d847091534399ec9e4be0ac9c6bdc8d418b
sbx_user1051 <sbx_user1051@169.254.16.175>
2025-04-14 14:57:20 +0000

    Mon Apr 14 14:57:20 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,12 +2,31 @@ class CheckoutSolution:
     ALLOWED_SKUS: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50, 'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50 }
     def checkout(self, skus: str) -> int:
-        # Return -1 for error if no string or if it contains anything that isn't ABCD
+        # Return -1 for error if no string or if it contains anything that isn't in ALLOWED_SKUS
         
         if not set(skus) <= set(self.ALLOWED_SKUS):
             return -1
 
-        {sku: skus.count(sku) for sku in set(self.ALLOWED_SKUS)}
+        # Count number of each letter
+        sku_counts = {sku: skus.count(sku) for sku in set(self.ALLOWED_SKUS)}
+
+        # Apply freebie count reductions
+        #2E get one B free
+        sku_counts['B'] = max(sku_counts['B'] - sku_counts['E'] // 2, 0)
+        #3N get one M free
+        sku_counts['M'] = max(sku_counts['M'] - sku_counts['N'] // 3, 0)
+        #3R get one Q free
+        sku_counts['Q'] = max(sku_counts['Q'] - sku_counts['R'] // 2, 0)
+
+        # Calculate price for each SKU
+        
+
+
+        # Total it all
+
+        # Return it
+
+
 
         # Count number of each letter
         num_a = skus.count("A")
@@ -42,59 +61,31 @@ class CheckoutSolution:
         return charge_a + charge_b + charge_c + charge_d + charge_e + charge_f + charge_a_discount_3 + charge_a_discount_5 + charge_b_discount + charge_f_discount
 
 
-{'A': 50
- 'B': 30
- 'C': 20
- 'D': 15
- 'E': 40
- 'F': 10
- 'G': 20
- 'H': 10
- 'I': 35
- 'J': 60
- 'K': 80
- 'L': 90
- 'M': 15
- 'N': 40
- 'O': 10
- 'P': 50
- 'Q': 30
- 'R': 50
- 'S': 30
- 'T': 20
- 'U': 40
- 'V': 50
- 'W': 20
- 'X': 90
- 'Y': 10
- 'Z': 50
-
-
 # +------+-------+------------------------+
 # | Item | Price | Special offers         |
 # +------+-------+------------------------+
-# | A    | 50    | 3A for 130, 5A for 200 |
-# | B    | 30    | 2B for 45              |
+# | A    | 50    | 3A for 130, 5A for 200 | multibuy discount
+# | B    | 30    | 2B for 45              | multibuy discount
 # | C    | 20    |                        |
 # | D    | 15    |                        |
-# | E    | 40    | 2E get one B free      |
-# | F    | 10    | 2F get one F free      |
+# | E    | 40    | 2E get one B free      | freebie
+# | F    | 10    | 2F get one F free      | multibuy discount
 # | G    | 20    |                        |
-# | H    | 10    | 5H for 45, 10H for 80  |
+# | H    | 10    | 5H for 45, 10H for 80  | multibuy discount
 # | I    | 35    |                        |
 # | J    | 60    |                        |
-# | K    | 80    | 2K for 150             |
+# | K    | 80    | 2K for 150             | multibuy discount
 # | L    | 90    |                        |
 # | M    | 15    |                        |
-# | N    | 40    | 3N get one M free      |
+# | N    | 40    | 3N get one M free      | freebie
 # | O    | 10    |                        |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
-# | R    | 50    | 3R get one Q free      |
+# | P    | 50    | 5P for 200             | multibuy discount
+# | Q    | 30    | 3Q for 80              | multibuy discount
+# | R    | 50    | 3R get one Q free      | freebie
 # | S    | 30    |                        |
 # | T    | 20    |                        |
-# | U    | 40    | 3U get one U free      |
-# | V    | 50    | 2V for 90, 3V for 130  |
+# | U    | 40    | 3U get one U free      | multibuy discount
+# | V    | 50    | 2V for 90, 3V for 130  | multibuy discount
 # | W    | 20    |                        |
 # | X    | 90    |                        |
 # | Y    | 10    |                        |
