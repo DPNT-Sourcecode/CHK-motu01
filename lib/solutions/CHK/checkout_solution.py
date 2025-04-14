71f9c77608653405bc2bcc951e901ee532681d5f
sbx_user1051 <sbx_user1051@169.254.16.175>
2025-04-14 14:04:14 +0000

    Mon Apr 14 14:04:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,24 @@
 
 class CheckoutSolution:
-
     # skus = unicode string
     def checkout(self, skus: str) -> int:
-        
+        # Return -1 for error if no string or if it containts anything that isn't ABCD
+        ALLOWED_SKUS: str = "ABCD"
+        if not skus or set(skus) <= set(ALLOWED_SKUS):
+            return -1
+
+        # Count number of each letter
+        num_a = skus.count("A")
+        num_b = skus.count("B")
+        num_c = skus.count("C")
+        num_d = skus.count("D")
+
+        # Check for discounts
+        num_a_discount, num_a = num_a /
+
+        # Add stuff up
+
+        # Return charge
 
 # +------+-------+----------------+
 # | Item | Price | Special offers |
@@ -13,4 +28,5 @@ class CheckoutSolution:
 # | C    | 20    |                |
 # | D    | 15    |                |
 # +------+-------+----------------+
+
 
